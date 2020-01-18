dac5b116a60bc191f69c7277d4bdf921271e2de7
Gamzat Asadulaev <asadulaevgamzat@gmail.com>
2020-01-19 01:33:01 +0300

    add project in github

---
+++ src/com/javalesson/oop/constructors/ConstructorsMain.java
@@ -0,0 +1,6 @@
+package com.javalesson.oop.constructors;
+
+
+public class ConstructorsMain {
+    
+}
---
+++ src/com/javalesson/oop/oop/primer.java
@@ -0,0 +1,17 @@
+package com.javalesson.oop.oop;
+
+public class primer {
+    public static void main(String[] args) {
+        System.out.println("Добавленная вода");
+        //System.out.println("Добавлен вода");
+        //addBlock();
+    }
+
+    public static void addWater(){
+
+    }
+
+//    public static void addWater() {
+//
+//    }
+}
