44a319077988c109e319dcbdb18c1a5d1bc50105
Gamzat Asadulaev <asadulaevgamzat@gmail.com>
2020-01-19 01:50:47 +0300

    Проект запулен

--- src/com/javalesson/oop/oop/primer.java
+++ src/com/javalesson/oop/oop/primer.java
@@ -3,5 +3,6 @@ package com.javalesson.oop.oop;
 public class primer {
     public static void main(String[] args) {
         System.out.println("Добавленны изменения в main");
+        System.out.println("Запуллен проект");
     }
 }
