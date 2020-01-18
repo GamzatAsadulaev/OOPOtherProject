95e9f579e1bd593aa91c0fea5ccf81d11e300286
Gamzat Asadulaev <asadulaevgamzat@gmail.com>
2020-01-19 01:36:44 +0300

    add changes in main function

--- src/com/javalesson/oop/oop/primer.java
+++ src/com/javalesson/oop/oop/primer.java
@@ -2,16 +2,6 @@ package com.javalesson.oop.oop;
 
 public class primer {
     public static void main(String[] args) {
-        System.out.println("Добавленная вода");
-        //System.out.println("Добавлен вода");
-        //addBlock();
+        System.out.println("Добавленны изменения в main");
     }
-
-    public static void addWater(){
-
-    }
-
-//    public static void addWater() {
-//
-//    }
 }
